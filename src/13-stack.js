3d3fb0749564d1bdc8c3b9e90deb9f0a2ed97fea
Newshinsky <72308491+Newshinsky@users.noreply.github.com>
2021-04-11 08:31:10 +0300

    feat: implement stack function

--- src/13-stack.js
+++ src/13-stack.js
@@ -11,16 +11,20 @@
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new Error('Not implemented');
+  constructor() {
+    this.element = [];
+  }
+
+  push(elements) {
+    this.element.push(elements);
   }
 
   pop() {
-    throw new Error('Not implemented');
+    return this.element.pop();
   }
 
   peek() {
-    throw new Error('Not implemented');
+    return this.element[this.element.length - 1];
   }
 }
 
